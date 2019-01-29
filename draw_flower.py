2695d2b8829d1b64de06c38c8e2846898fa7b82e
taukir <33090525+ataukir@users.noreply.github.com>
2019-01-29 23:31:05 +0600

    drawing square-circle-triangle

---
+++ draw_flower.py
@@ -0,0 +1,43 @@
+import turtle
+
+def draw_flower(ink):
+    ink.up()
+    ink.home()
+    for degrees in range(10, 370, 10):
+        ink.down()
+        ink.forward(200)
+        ink.up()
+        ink.home()  # resets angle to 0 degrees
+        ink.left(degrees)
+
+
+def draw_stem(stem):
+
+    stem.goto(1, -400)
+    stem.home()
+    stem.goto(5, -400)
+    stem.home()
+    stem.goto(3, -400)
+
+
+def flower():
+
+    window = turtle.Screen()
+    window.bgcolor('green')
+
+    stem = turtle.Turtle()
+    stem.color('yellow')
+    stem.speed('fast')
+
+    draw_stem(stem)
+
+    flo = turtle.Turtle()
+    flo.color('white')
+    flo.speed('fastest')
+
+    draw_flower(flo)
+
+    window.exitonclick()
+
+
+flower()
